3eb5558b8225f6240cf1b75b0fac13046208b160
arijitkar <arijitkar29@gmail.com>
2023-04-05 22:38:26 +0530

    first commit

---
+++ api/app.py
@@ -0,0 +1,14 @@
+# declare functions and classess here
+# import statements here
+
+import index
+
+
+def main():
+    app = index.app
+    app.run(debug=True)
+# main program
+
+
+if __name__ == '__main__':
+    main()
